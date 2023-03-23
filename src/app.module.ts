f20dbd8ad1023373e05105c13d7642f48f057523
Erohal <erohalthebest@gmail.com>
2023-03-23 14:50:05 +0800

    feat: 添加对Swagger的支持 style: 调整代码格式

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { ChatModule } from './chat/chat.module';
 
 @Module({
-  imports: [UserModule, AuthModule, ChatModule, ConfigModule.forRoot()],
-  controllers: [],
-  providers: [],
+  imports: [
+    UserModule,
+    AuthModule,
+    ChatModule,
+    ConfigModule.forRoot(),
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('ChatGPT-API')
+    .setDescription('Hi there, this is the swagger docs of this api')
+    .setVersion('0.0.1')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
