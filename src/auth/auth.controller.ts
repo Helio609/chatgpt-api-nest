d2047c4fcec9b8e1d02791315735809390c03998
Erohal <erohalthebest@gmail.com>
2023-03-23 14:48:15 +0800

    feat: 适配Swagger，添加ApiBody

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,13 @@
 import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { ApiBody, ApiTags } from '@nestjs/swagger';
+import { CreateUserDto } from 'src/user/dto/create-user.dto';
 import { UserService } from 'src/user/user.service';
 import { AuthService } from './auth.service';
 import { CurrentUser } from './decorators/current-user.decorator';
+import { LoginDto } from './dto/login.dto';
 
+@ApiTags('auth')
 @Controller('auth')
 export class AuthController {
   constructor(
@@ -13,12 +17,13 @@ export class AuthController {
 
   @Post('login')
   @UseGuards(AuthGuard('local'))
+  @ApiBody({ type: LoginDto })
   login(@CurrentUser() user) {
     return this.authService.login(user);
   }
 
   @Post('register')
-  register(@Body() dto: { username: string; password: string }) {
-    return this.userService.register(dto.username, dto.password);
+  register(@Body() dto: CreateUserDto) {
+    return this.userService.register(dto);
   }
 }
